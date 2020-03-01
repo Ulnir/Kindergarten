39f8ac4b5fc8706776bc07339185787e577cc40a
Kang <violet_idea@naver.com>
2020-03-02 01:47:12 +0900

    프로토타입 완성

---
+++ Assets/DragDrop.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using HutongGames.PlayMaker;
+
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void Start()
+    {
+      
+            canvas = GameObject.Find("Canvas").GetComponent<Canvas>();                
+      
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log(GetComponent<RectTransform>().anchoredPosition);
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+
+        if (GetComponent<RectTransform>().anchoredPosition.x < -500) {
+
+            Destroy();
+        }
+
+        if (GetComponent<RectTransform>().anchoredPosition.x > 500)
+        {
+            Destroy();
+        }
+
+        if (GetComponent<RectTransform>().anchoredPosition.y < -900)
+        {
+            Destroy();
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+        
+    }
+
+    public void Destroy()
+    {
+        if (this.tag == "Dust") {
+            FsmVariables.GlobalVariables.GetFsmInt("Int_CurrentDust").Value--;
+        }
+
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        this.tag = "Untagged";
+        this.gameObject.SetActive(false);
+
+
+        
+    }
+}
---
+++ Assets/ItemSlot.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using HutongGames.PlayMaker;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    public GameObject Item;
+    public GameObject GameManager;
+    PlayMakerFSM MainFSM;
+    CanvasGroup canvasGroup;
+    
+        
+
+    public void Start()
+    {
+        MainFSM = GameManager.GetComponent<PlayMakerFSM>();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null) {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            Item = eventData.pointerDrag.gameObject;
+
+            if (Item.tag == "Meat")
+            {
+                MainFSM.SendEvent("EatMeat");
+                Destroy();
+            }
+
+            if (Item.tag == "Vege")
+            {
+                MainFSM.SendEvent("EatVege");
+                Destroy();
+                
+            }
+
+            if (Item.tag == "Milk")
+            {
+                MainFSM.SendEvent("EatMilk");
+                Destroy();
+            }
+
+            if (Item.tag == "Ball")
+            {
+                MainFSM.SendEvent("EatBall");
+                Destroy();
+
+            }
+
+            if (Item.tag == "Book")
+            {
+                MainFSM.SendEvent("EatBook");
+                Destroy();
+            }
+
+            if (Item.tag == "Game")
+            {
+                MainFSM.SendEvent("EatGame");
+                Destroy();
+
+            }
+
+            if (Item.tag == "Dust")
+            {
+                MainFSM.SendEvent("EatDust");
+                Destroy();
+
+            }
+
+        }
+    }
+
+    public void Destroy()
+    {
+        Item.tag = "Untagged";
+        Item.SetActive(false);
+        canvasGroup = Item.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+    }
+}
