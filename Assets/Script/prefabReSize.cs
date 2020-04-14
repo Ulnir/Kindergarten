95ea0f28e83ad8838435a4bf8ab2fc669b35ad0d
강_판교 <violet_idea@naver.com>
2020-04-14 17:59:56 +0900

    타이틀화면작업, 프리팹 해상도 작업 중

---
+++ Assets/Script/prefabReSize.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabReSize : MonoBehaviour
+{
+    public GameObject parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        this.transform.SetParent(parent.transform, false);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
